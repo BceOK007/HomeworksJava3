d1b0fa72a096e08fc2cf865c2864cf8fa7fe492b
BceOK <artrozhko@gmail.com>
2021-04-29 17:49:52 +0300

    Гонки. Готовая версия

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,16 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     private Race race;
     private int speed;
     private String name;
+    private  CyclicBarrier cyclicBarrier;
+    private static boolean isWinnerFound = false;
+    private Lock lock = new ReentrantLock();
 
     public String getName() {
         return name;
@@ -12,11 +20,12 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier cyclicBarrier) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cyclicBarrier = cyclicBarrier;
     }
 
     @Override
@@ -25,11 +34,29 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            //1. машина готова, уменьшаем кол-во ожидаемых потоков на 1
+            cyclicBarrier.await();
+            //4. Ждем команды, что остальные машины и основной поток будут готовы
+            cyclicBarrier.await();
+
+
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+
+        //Определяем победителя.
+        //Если получилось зайти и победитель еще не найден, заходим и объявляем себя победителем
+        if (lock.tryLock() && !isWinnerFound) {
+            isWinnerFound = true;
+            System.out.println(String.format("%s - WIN", name));
+            lock.unlock();
+        }
+
+        //5. машина закончила гонку
+        cyclicBarrier.await();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,39 @@
+import java.util.concurrent.*;
+
 public class Main {
 
     public static final int CARS_COUNT = 4;
 
     public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT + 1);
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrier);
         }
+
+        //Запускаем все машины
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        try {
+            //2. ждем пока отработают потоки подготовки всех машин
+            cyclicBarrier.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            //3. командуем, что все готовы
+            cyclicBarrier.await();
+            //6. ждем когда все машины закончат гонку
+            cyclicBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
